5a2d1f6173f0d9e27c8f771f06f2fe6a49f350ec
kolyan <vkont4@gmail.com>
2018-01-30 13:37:29 +0200

    Added initial version of sign up

---
+++ app/models/security/SignUp.scala
@@ -0,0 +1,4 @@
+package models
+
+
+case class SignUp(identifier: String, password: String, email: String, firstName: String, lastName: String)
---
+++ test/SignUpSpec.scala
@@ -0,0 +1,6 @@
+/**
+  * @author Mykola Yashchenko
+  */
+class SignUpSpec {
+
+}
